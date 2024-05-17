0facb962c8c4489deddc038de451bc8004de1555
Challa200Santhosh <161731044+Challa200Santhosh@users.noreply.github.com>
2024-05-17 09:00:52 +0000

    done the job

--- 3_Implementation/claculator.c
+++ 3_Implementation/claculator.c
@@ -2,16 +2,18 @@
 
 int main()
 {
-    int num1, num2, add, subb, mul;
+    int num1, num2, add, subb, mul,DIV;
     printf("Enter two numbers");
     scanf("%d %d",&num1, &num2);
 
     add = num1 + num2;
     subb = num1 - num2;
     mul = num1 * num2;
+    DIV = num1/num2;
 
     printf("Addition of numbers %d",add);
     printf("Subtraction of numbers %d",subb);
     printf("Multiplication of numbers %d",mul);
+    printf("division of the numbers %d",DIV);
     return 0;
 }
