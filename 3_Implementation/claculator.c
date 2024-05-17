228c7c4005486e8183953ff4c91def9b6bf13890
siva6014 <161362372+siva6014@users.noreply.github.com>
2024-05-17 07:07:26 +0000

    Added sub, mul and add

--- 3_Implementation/claculator.c
+++ 3_Implementation/claculator.c
@@ -2,12 +2,16 @@
 
 int main()
 {
-    int num1, num2, result;
+    int num1, num2, add, sub, mul;
     printf("Enter two numbers");
     scanf("%d %d",&num1, &num2);
-    
-    result = num1 - num2;
 
-    printf("%d",result);
+    add = num1 + num2;
+    sub = num1 - num2;
+    mul = num1 * num2;
+
+    printf("Addition of numbers %d",add);
+    printf("Subtraction of numbers %d",sub);
+    printf("Multiplication of numbers %d",mul);
     return 0;
 }
