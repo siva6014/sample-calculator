16b9af0bdaa3df2a7d3a187e71320d81a6f2e35e
Challa200Santhosh <161731044+Challa200Santhosh@users.noreply.github.com>
2024-05-17 09:08:40 +0000

    division

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   int num1 ,num2,result;
+   printf("Enter the numbers:  " " ");
+   scanf("%d %d",&num1,&num2);
+   result = num1 / num2;
+   printf("Sum of %d and %d is %d\n",num1 ,num2,result);
+   
+   
+    return 0;
+}
