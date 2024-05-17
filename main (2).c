b645d372b8c64c607f8124595be72a8a501eb461
AhmedBU21EECE0200026 <138860410+AhmedBU21EECE0200026@users.noreply.github.com>
2024-05-17 09:19:55 +0000

    done ,#2

---
+++ main (2).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2, result;
+    printf("Enter Two Numbers:\n");
+    scanf("%d %d",&num1, &num2);
+    result = num1 + num2;
+    printf("%d",result);
+    return 0;
+}
