5b29dbc9395b4bc3dd764285c815e13e8d4e61fc
siva6014 <161362372+siva6014@users.noreply.github.com>
2024-05-17 06:46:03 +0000

    testing

---
+++ 3_Implementation/claculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, result;
+    printf("Enter two numbers");
+    scanf("%d %d",&num1, &num2);
+    
+    result = num1 + num2;
+
+    printf("%d",result);
+    return 0;
+}
