7a10cde9c873503d07f16cb50a2706ca87eee4e1
ch pavankumar <pcheerla2@gitam.in>
2024-05-17 15:08:59 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 int main()
 {
    int num1 ,num2,result;
-   printf("Enter the numbers:  " " ");
+   printf("Enter the numbers:");
    scanf("%d %d",&num1,&num2);
    result = num1 / num2;
    printf("Sum of %d and %d is %d\n",num1 ,num2,result);
